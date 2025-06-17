a4da0fb37c1d64b51ec422ef31ffc99755e1fa06
Tooma81 <toomas.kaarel@gmail.com>
2025-06-17 13:29:52 +0300

    adding data into components

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
 const App = () => {
+  const expenses = [
+    {
+      date: new Date(2024, 10, 12),
+      title: 'New Book',
+      price: 30.99
+    },
+    {
+      date: new Date(2024, 10, 12),
+      title: 'New Jeans',
+      price: 99.99
+    }
+  ]
+
   return (
     <div>
-      <ExpenseItem />
+      <ExpenseItem data={expenses[0]}/>
+      <ExpenseItem data={expenses[1]}/>
     </div>
   );
 } 
